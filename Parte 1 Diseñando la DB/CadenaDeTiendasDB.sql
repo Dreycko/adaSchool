02bf01ee6a87ce02bf1e9db6ccfe414b6df797f1
Dreycko <edwin.toribio.j@gmail.com>
2023-09-29 23:16:31 -0500

    Dada la base de datos que diseñamos en la anterior etapa, escribe las consultas para crear cada una de las tablas en SQLite. La tabla stock debe tener una restricción de unicidad unique (sucursal_id, producto_id).

--- Parte 1 Diseñando la DB/CadenaDeTiendasDB.sql
+++ Parte 1 Diseñando la DB/CadenaDeTiendasDB.sql
@@ -1,68 +1,63 @@
--- Crear la base de datos
-CREATE DATABASE CadenaDeTiendasDB;
-
--- Usar la base de datos
-USE CadenaDeTiendasDB;
 
 -- Crear la tabla Categoria
 CREATE TABLE Categoria (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(255)
+    id INTEGER PRIMARY KEY,
+    nombre TEXT
 );
 
 -- Crear la tabla Producto
 CREATE TABLE Producto (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(255),
-    marca VARCHAR(255),
-    categoria_id INT,
-    precio_unitario DECIMAL(10, 2),
+    id INTEGER PRIMARY KEY,
+    nombre TEXT,
+    marca TEXT,
+    categoria_id INTEGER,
+    precio_unitario NUMERIC(10, 2),
     FOREIGN KEY (categoria_id) REFERENCES Categoria(id)
 );
 
 -- Crear la tabla Sucursal
 CREATE TABLE Sucursal (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(255),
-    direccion VARCHAR(255)
+    id INTEGER PRIMARY KEY,
+    nombre TEXT,
+    direccion TEXT
 );
 
 -- Crear la tabla Stock
 CREATE TABLE Stock (
-    id INT PRIMARY KEY,
-    sucursal_id INT,
-    producto_id INT,
-    cantidad INT,
-    UNIQUE KEY unique_stock (sucursal_id, producto_id),
+    id INTEGER PRIMARY KEY,
+    sucursal_id INTEGER,
+    producto_id INTEGER,
+    cantidad INTEGER,
+    UNIQUE (sucursal_id, producto_id),
     FOREIGN KEY (sucursal_id) REFERENCES Sucursal(id),
     FOREIGN KEY (producto_id) REFERENCES Producto(id)
 );
 
 -- Crear la tabla Cliente
 CREATE TABLE Cliente (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(255),
-    telefono VARCHAR(20)
+    id INTEGER PRIMARY KEY,
+    nombre TEXT,
+    telefono TEXT
 );
 
 -- Crear la tabla Orden
 CREATE TABLE Orden (
-    id INT PRIMARY KEY,
-    cliente_id INT,
-    sucursal_id INT,
+    id INTEGER PRIMARY KEY,
+    cliente_id INTEGER,
+    sucursal_id INTEGER,
     fecha DATE,
-    total DECIMAL(10, 2),
+    total NUMERIC(10, 2),
     FOREIGN KEY (cliente_id) REFERENCES Cliente(id),
     FOREIGN KEY (sucursal_id) REFERENCES Sucursal(id)
 );
 
 -- Crear la tabla Item
 CREATE TABLE Item (
-    id INT PRIMARY KEY,
-    orden_id INT,
-    producto_id INT,
-    cantidad INT,
-    monto_venta DECIMAL(10, 2),
+    id INTEGER PRIMARY KEY,
+    orden_id INTEGER,
+    producto_id INTEGER,
+    cantidad INTEGER,
+    monto_venta NUMERIC(10, 2),
     FOREIGN KEY (orden_id) REFERENCES Orden(id),
     FOREIGN KEY (producto_id) REFERENCES Producto(id)
 );
